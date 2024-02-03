77030b47e56702c72d99dedf967453a1cb3b77ec
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-02-03 01:00:37 +0100

    Feat: Add controllers for instructions andbg-sound  and make it interactive using JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,17 @@
+function showInstructions(){
+    let controllers = document.getElementById('controllers');
+    controllers.style.display='none';
+    let howToPlaySection = document.getElementById('game-instructions');
+    howToPlaySection.style.display='block';
+}
+
 function hideInstructions(){
+    let controllers = document.getElementById('controllers');
+    controllers.style.display='flex';
+    controllers.style.justifyContent='space-around';
+    controllers.style.alignItems='center';
+
     let howToPlaySection = document.getElementById('game-instructions');
     howToPlaySection.style.display='none';
 }
+
