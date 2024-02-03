f203490eecb67548d5064af61b359396900abee6
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-02-03 19:18:49 +0100

    Feat: debugging the hide and show fucntions in JS and take some style to the JS instead of CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
 function showInstructions(){
     let controllers = document.getElementById('controllers');
-    controllers.style.display='none';
+    controllers.style.display = 'none';
+    
     let howToPlaySection = document.getElementById('game-instructions');
     howToPlaySection.style.display='block';
+
+    let gameBoard=document.getElementById('game-board');
+    gameBoard.style.display='none';
+
+    let keyboard=document.getElementById('keyboard-container');
+    keyboard.style.display='none';
+
+    
 }
 
 function hideInstructions(){
@@ -10,6 +19,20 @@ function hideInstructions(){
     controllers.style.display='flex';
     controllers.style.justifyContent='space-around';
     controllers.style.alignItems='center';
+
+    let gameBoard=document.getElementById('game-board');
+    gameBoard.style.display='grid';
+    gameBoard.style.gridTemplateColumns="60px 60px 60px 60px 60px";
+    gameBoard.style.gridTemplateRows="65px 65px 65px 65px 65px 65px";
+    gameBoard.style.justifyContent="space-evenly";
+    gameBoard.style.alignItems="center"
+    //gameBoard.style.gridGap="10px";
+
+    let keyboard=document.getElementById('keyboard-container');
+    keyboard.style.display='flex';
+    keyboard.style.flexDirection = 'column';
+    keyboard.style.justifyContent ='space-between';
+    keyboard.style.alignItems = 'center';
 
     let howToPlaySection = document.getElementById('game-instructions');
     howToPlaySection.style.display='none';
